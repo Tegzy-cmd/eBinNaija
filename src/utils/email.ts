bf17fcee8e570b3180d2cc82da252b18d22c1681
Tegzy-cmd <omorakabenjamin3@gmail.com>
2025-09-26 19:42:07 +0100

    fixed secret leak

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -15,7 +15,7 @@ const transporter = nodemailer.createTransport({
 
 export const sendEmail = async (to: string, subject: string, text: string, html?: string) => {
   await transporter.sendMail({
-    from: `support@bitminersfx.com`,
+    from: process.env.SMTP_USER,
     to,
     subject,
     text,
